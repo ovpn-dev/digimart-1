9351cc2edc325134941fac8ad9dd8ec9f31eb62c
draysongz <dharmiegra4@gmail.com>
2023-07-01 15:08:23 +0100

    forgot modified

--- src/Components/Forgot.jsx
+++ src/Components/Forgot.jsx
@@ -13,48 +13,16 @@ const Forgot = () => {
   const [email, setEmail] =useState('')
   // const navigate = useNavigate()
 
-  const generateVerificationCode = () => {
-    const codeLength = 4;// Length of the verification code
-    const digits = '0123456789'; // Allowed digits for the code
-  
-    let code = '';
-    for (let i = 0; i < codeLength; i++) {
-      const randomIndex = Math.floor(Math.random() * digits.length);
-      code += digits[randomIndex];
-    }
-  
-    return code;
-  };
-
-const saveVerificationCodeToFirestore = async (email, verificationCode) => {
-  const db = getFirestore(app)
-  const verificationCodesCollectionRef = collection(db, "verificationCodes"); // Replace "verificationCodes" with the actual collection name
-
-  try {
-    const verificationCodeDocRef = doc(verificationCodesCollectionRef, email); // Use the user's email as the document ID
-
-    await setDoc(verificationCodeDocRef, { code: verificationCode });
-    console.log("Verification code saved to Firestore successfully.");
-  } catch (error) {
-    console.error("Error saving verification code to Firestore:", error);
-    // Handle error (e.g., show an error message to the user)
-  }
-};
-
-
   
   const handleClick = async (e) => {
   e.preventDefault();
   const auth = getAuth(app);
-  const verificationCode = generateVerificationCode();
   const actionCodeSettings = {
-    url: `https://digimart-exchange.vercel.app/reset?code=${verificationCode}`,
+    url: `https://digimart-exchange.vercel.app/reset`,
     handleCodeInApp: true,
   };
-
   try {
     const sent = await sendPasswordResetEmail(auth, email, actionCodeSettings);
-    const savedCode =await saveVerificationCodeToFirestore(email, verificationCode);
     toast.success("Email verification sent");
     console.log(sent, savedCode)
   } catch (error) {
