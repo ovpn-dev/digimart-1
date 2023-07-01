f2463b5512bc1bdf274d4d20bdad73b62c4ba4fe
draysongz <dharmiegra4@gmail.com>
2023-07-01 13:59:30 +0100

    forgot

--- src/Components/Forgot.jsx
+++ src/Components/Forgot.jsx
@@ -1,16 +1,69 @@
 import React, {useState} from 'react'
 import logo from './assets/logo.png'
 import './css/forgot.css'
-import { useNavigate } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { app } from './firebase/Firebase';
+import {toast} from 'react-toastify'
+import { collection, doc, setDoc } from "firebase/firestore"
+import { getFirestore } from 'firebase/firestore';
+
 
 const Forgot = () => {
   const [email, setEmail] =useState('')
-  const navigate = useNavigate()
+  // const navigate = useNavigate()
 
-  const handleClick = async (e)=>{
-    e.preventDefault()
-    navigate('/reset')
+  const generateVerificationCode = () => {
+    const codeLength = 4;// Length of the verification code
+    const digits = '0123456789'; // Allowed digits for the code
+  
+    let code = '';
+    for (let i = 0; i < codeLength; i++) {
+      const randomIndex = Math.floor(Math.random() * digits.length);
+      code += digits[randomIndex];
+    }
+  
+    return code;
+  };
+
+const saveVerificationCodeToFirestore = async (email, verificationCode) => {
+  const db = getFirestore(app)
+  const verificationCodesCollectionRef = collection(db, "verificationCodes"); // Replace "verificationCodes" with the actual collection name
+
+  try {
+    const verificationCodeDocRef = doc(verificationCodesCollectionRef, email); // Use the user's email as the document ID
+
+    await setDoc(verificationCodeDocRef, { code: verificationCode });
+    console.log("Verification code saved to Firestore successfully.");
+  } catch (error) {
+    console.error("Error saving verification code to Firestore:", error);
+    // Handle error (e.g., show an error message to the user)
   }
+};
+
+
+  
+  const handleClick = async (e) => {
+  e.preventDefault();
+  const auth = getAuth(app);
+  const verificationCode = generateVerificationCode();
+  const actionCodeSettings = {
+    url: `https://digimart-exchange.vercel.app/reset?code=${verificationCode}`,
+    handleCodeInApp: true,
+  };
+
+  try {
+    const sent = await sendPasswordResetEmail(auth, email, actionCodeSettings);
+    const savedCode =await saveVerificationCodeToFirestore(email, verificationCode);
+    toast.success("Email verification sent");
+    console.log(sent, savedCode)
+  } catch (error) {
+    error.message === "Firebase: Error (auth/user-not-found)."? toast.error("User not found") : toast.error(error.message);
+    console.log(error.message);
+  }
+};
+
+  
   return (
     <div className='fgCont'>
       <div className="main">
@@ -25,7 +78,7 @@ const Forgot = () => {
           <span ><p className='back'>back into your account</p></span>
           </div>
          
-          <input type="email" onChange={(e)=> setEmail(e.target.value)} className="forgotMail" placeholder='Email address' />
+          <input type="email" value={email} onChange={(e)=> setEmail(e.target.value)} className="forgotMail" placeholder='Email address' />
           <button className="submit-btn" onClick={handleClick}>SUBMIT</button>
         </div>
       </div>
--- src/Components/Reset.jsx
+++ src/Components/Reset.jsx
@@ -1,42 +1,87 @@
-import React, {useState} from 'react'
-import logo from './assets/logo.png'
-import './css/reset.css'
-import {Link} from 'react-router-dom'
-
+import React, { useState, useEffect } from 'react';
+import logo from './assets/logo.png';
+import './css/reset.css';
+import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { getAuth, verifyPasswordResetCode } from 'firebase/auth';
+import {toast} from 'react-toastify'
+import { app } from './firebase/Firebase';
 
 const Reset = () => {
-  const [password, setPassword] = useState('')
-  const [confirm, setConfirm] = useState('')
+  const [password, setPassword] = useState('');
+  const [confirm, setConfirm] = useState('');
+  const [isVerified, setIsVerified] = useState(false);
+  const [isLoading, setIsLoading] = useState(true); // State to track loading status
 
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const actionCode = searchParams.get('code');
+  const auth = getAuth(app);
 
+  useEffect(() => {
+    const verifyActionCode = async () => {
+      if (actionCode) {
+        try {
+          const email = await verifyPasswordResetCode(auth, actionCode);
+          // The code is valid, and the associated email is retrieved
+          const accountEmail = email;
+          // Proceed with displaying the password reset form or any other necessary logic
+          setIsVerified(true)
+          console.log(accountEmail)
+          
+        } catch (error) {
+          // Handle invalid or expired code
+          console.error('Error verifying password reset code:', error);
+        } finally {
+          setIsLoading(false); // Set loading status to false after verification
+          toast.success('code verified')
+          
+        }
+      }
+    };
 
-  const handleClick = async (e)=>{
-    e.preventDefault()
-    console.log(password)
-    console.log(confirm)
+    verifyActionCode();
+  }, [auth, actionCode]);
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    console.log(password);
+    console.log(confirm);
+  };
+
+  if (isLoading) {
+    // Display a loading spinner while verifying the code
+    return <div>Loading...</div>;
   }
+
+  if (!isVerified) {
+    // Display a blank page or a message when not verified
+    return <div>Invalid code or expired link.</div>;
+  }
+
   return (
     <div className='resetCont'>
       <div className="main">
-      <div className="digi">
-        <img src={logo} alt="digimart" className="mainlogo" />
+        <div className="digi">
+          <img src={logo} alt="digimart" className="mainlogo" />
         </div>
 
         <div className="mainReset">
           <h1 className="resetHeader">Reset your password</h1>
           <div className="eDescriptionDiv">
-          <p className="eDescription">To reset your password, enter a new password</p> <br/>
-          <span ><p className='box'>in the box below</p></span>
+            <p className="eDescription">To reset your password, enter a new password</p>
+            <br />
+            <span><p className='box'>in the box below</p></span>
           </div>
-         
-          <input type="password" onChange={(e)=> setPassword(e.target.value)} className="resetPass" placeholder='New password' />
-          <input type="password" onChange={(e)=> setConfirm(e.target.value)}placeholder='Re-type new password' className="retype" />
+
+          <input type="password" onChange={(e) => setPassword(e.target.value)} className="resetPass" placeholder='New password' />
+          <input type="password" onChange={(e) => setConfirm(e.target.value)} placeholder='Re-type new password' className="retype" />
           <button className="submit-btn" onClick={handleClick}>SUBMIT</button>
           <p className="loginD">Didn't get a mail? <Link className='log' to='/register'>Resend it</Link></p>
+        </div>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Reset
+export default Reset;
